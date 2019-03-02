0a137af1652e61d941477c2dfa7cff187b5c7da9
Recep Yüksel <3923029+bmrecep@users.noreply.github.com>
2019-03-02 13:25:27 +0300

    hibernate eklendi

--- SBProject/src/main/java/com/example/demo/SbProjectApplication.java
+++ SBProject/src/main/java/com/example/demo/SbProjectApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.demo.entities")
 public class SbProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbProjectApplication.class, args);
-		//dsafs
 	}
 
 }
-
